b2eb6c90d3ad284e0dcad49c458c5275400cf46a
JakubFr4czek <jakubfraczek.m8@gmail.com>
2025-11-12 12:49:08 +0100

    Remove -t from dockerize

--- dockerize.sh
+++ dockerize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v $(pwd):/var/yocto --rm --workdir /var/yocto --name yoctobuilder ubuntu:22.04 bash
+docker run -i -v $(pwd):/var/yocto --rm --workdir /var/yocto --name yoctobuilder ubuntu:22.04 bash
