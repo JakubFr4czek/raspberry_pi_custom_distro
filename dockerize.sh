81fbb579cd424a8c4cff27a7562975b2fbd94f39
JakubFr4czek <jakubfraczek.m8@gmail.com>
2025-11-12 12:49:46 +0100

    Add sudo to dockerize

--- dockerize.sh
+++ dockerize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -i -v $(pwd):/var/yocto --rm --workdir /var/yocto --name yoctobuilder ubuntu:22.04 bash
+sudo docker run -i -v $(pwd):/var/yocto --rm --workdir /var/yocto --name yoctobuilder ubuntu:22.04 bash
